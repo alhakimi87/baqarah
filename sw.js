3c01857fd65f12bfac02806daef1819c4f462463
alhakimi87 <44662310+alhakimi87@users.noreply.github.com>
2026-09-20 14:03:23 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service worker: يحفظ واجهة التطبيق والخطوط لتعمل دون إنترنت.
 // الصوت والنص يديرهما التطبيق نفسه (Cache API + localStorage).
-const SHELL = 'baq-shell-v1';
+const SHELL = 'baq-shell-v2';
 const FONTS = 'baq-fonts-v1';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
